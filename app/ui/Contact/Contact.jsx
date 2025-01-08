5383c76ded5c4beb8319fa7f032fb01d97a607a7
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2025-01-08 12:17:52 -0500

    cleaned up code

--- app/ui/Contact/Contact.jsx
+++ app/ui/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+import { EnvelopeIcon } from '@heroicons/react/24/outline'
 
 export default function Contact() {
     return (
