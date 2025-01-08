07d0ca06dc487cbb059ceb131f9b5b615435a7b9
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2025-01-08 12:39:47 -0500

    updated keywords

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,8 @@ import Global from "./styleComponents/global";
 
 export const metadata = {
   title: 'Music | Kaileb Hammontree',
-  description: 'Discover Kaileb Hammontree and listen to his music now. Connect with him through email and various social platforms.'
+  description: 'Discover Kaileb Hammontree and listen to his music now. Connect with him through email and various social platforms.',
+  keywords: 'Kaileb, Hammontree, For You, Hey You, Music, Singer, Songwriter, Spotify, iHeartRadio'
 }
 export default function RootLayout({ children }) {
   return (
